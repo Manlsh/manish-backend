0b98a801df0ebd78748bd56e4b73e4a6a76bf788
Manish <manlshrocks@gmail.com>
2024-09-28 12:20:40 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,7 +34,7 @@ const userSchema = new Schema({
   },
   watchHistory: [
     {
-      type: String.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Video"
     }
   ],
@@ -50,7 +50,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
   if (!this.isModified("Password")) return next()
     
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
